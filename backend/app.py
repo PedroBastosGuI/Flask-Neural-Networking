c62573e77655d13e67ba32fbd33f87c21e40296c
Pedro Guilherme <pedrobastos6540@gmail.com>
2022-01-18 13:47:36 -0300

    EndPoint configuration

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template
 from reverseProxy import proxyRequest
+from flask import Flask, render_template, request
 
 MODE = os.getenv('FLASK_ENV')
 DEV_SERVER_URL = 'http://localhost:3000/'
@@ -18,3 +19,17 @@ def index(path=''):
         return proxyRequest(DEV_SERVER_URL, path)
     else:
         return render_template("index.html")
+
+
+#criando endpoint para receber imagem que
+
+@app.route('/classify', methods=['POST'])
+
+def classify():
+    if(request.files['image']):
+        file = request.files['image']
+
+        result = classifyImage(file)
+        print('Model Classification:' + result)
+
+        return result
